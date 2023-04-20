2450fc48829a632e035dde268a3dac4bdfe8316c
Alexis Delgado <adelgado4419@gmail.com>
2023-04-19 20:44:24 -0700

    added more backend files

---
+++ backend/controllers/authentication.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
+const { User } = db
+
+router.post('/', async (req, res) => {
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: `Could not find a user with the provided username and password`
+        })
+    } else {
+        req.session.userID = user.userID
+        res.json({ user })
+    }
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userID)
+    try {
+    //     let user = await User.findOne({
+    //         where: {
+    //             userId:
+    //         }
+    //     })
+    //     res.json(user)
+    } catch {
+    //     res.json(null)
+    }
+    })
+})
+module.exports = router
