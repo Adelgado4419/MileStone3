1b2e9558f8f2c2112b186cf003338ac7fed841b2
yalfarosullivan@yahoo.com <Yara Alfaro-Sullivan>
2023-04-22 11:56:18 -0700

    added sass files

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.css';
 import Home from './Home';
 import Dashboard from './Dashboard';
 import CreateAccount from './CreateAccount';
