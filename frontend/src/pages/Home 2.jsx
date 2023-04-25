159b6ea610cb38c763bd22e1af664b594a9f8e76
Alexis Delgado <adelgado4419@gmail.com>
2023-04-24 18:26:57 -0700

    added photos to canva document

---
+++ frontend/src/pages/Home 2.jsx
@@ -0,0 +1,15 @@
+import Nav from "../components/nav"
+
+function Home() {
+  return (
+    <div>
+        <Nav/>
+        <p>
+          Fantasy Tinder
+        </p>
+
+    </div>
+  );
+}
+
+export default Home;
