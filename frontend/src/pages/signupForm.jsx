0272f3cfa345e393b0bbfd8d91526be3e01f6b6a
jake <jakebrown536@gmail.com>
2023-04-26 19:15:52 -0600

    main up to date

--- frontend/src/pages/signupForm.jsx
+++ frontend/src/pages/signupForm.jsx
@@ -19,7 +19,7 @@ function SignUpForm() {
 	async function handleSubmit(e) {
 		e.preventDefault()
 		navigate('/')
-		await fetch(`http://localhost:4005/api/Users`, {
+		const response = await fetch(`http://localhost:4005/api/Users`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
