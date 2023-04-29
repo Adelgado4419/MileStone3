c87c16ccc77d1f3409d6aa0713840e030bb2295c
Alexis Delgado <adelgado4419@gmail.com>
2023-04-29 10:38:38 -0700

    removed loggin true from configjs, copied and pasted credentials to all developments

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,20 +7,19 @@ module.exports = {
     database: "postgres",
     host: "db.cxjotagktppfnwfypixu.supabase.co",
     dialect: "postgres",
-    logging: true,
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host:  process.env.DB_HOST,
-    dialect: "postgres"
+    username: "postgres",
+    password: "Y4na8EstEs$",
+    database: "postgres",
+    host: "db.cxjotagktppfnwfypixu.supabase.co",
+    dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,  
-    dialect: "postgres"
+    username: "postgres",
+    password: "Y4na8EstEs$",
+    database: "postgres",
+    host: "db.cxjotagktppfnwfypixu.supabase.co",
+    dialect: "postgres",
   }
 }
