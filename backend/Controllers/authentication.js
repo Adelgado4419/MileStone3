38bc82851fe1c99a3339cbd2b13126f7fd0f72e3
jake <jakebrown536@gmail.com>
2023-04-30 13:46:17 -0600

    jwt start

--- backend/Controllers/authentication.js
+++ backend/Controllers/authentication.js
@@ -1,6 +1,7 @@
 const auth = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 const { User } = db
 
 auth.post('/', async (req, res) => {
